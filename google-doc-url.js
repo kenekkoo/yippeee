15838fa1f4cb742d3267ceaaa8eee76929e4500e
kenekkoo <149088359+kenekkoo@users.noreply.github.com>
2023-10-26 17:20:30 +0200

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1AO6XHL_0JafWZF4KEejkdDNqfuZWUk3SlNlQ6MjlRFM/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1bnZG-1gkomP7btloj10JuPlhVjKzdc1iCHnWmXE78jc/edit#gid=0'
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
